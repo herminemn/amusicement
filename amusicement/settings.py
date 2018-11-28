5684f1013e62ff117345c00378ce5fb0b0712dfe
herminemn <hermine.mn@gmail.com>
2018-11-28 21:55:39 +0400

    changes for preparing heroku

--- amusicement/settings.py
+++ amusicement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/pics/'
 MEDIA_ROOT = BASE_DIR
+
+django_heroku.settings(locals())
